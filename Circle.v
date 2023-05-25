93fd10b86678a276e32a13a0a4f3cf49a85e56a8
Pallabi Sree Sarker <p.s.p.sarker@student.tudelft.nl>
2023-05-25 14:51:31 +0200

    Defined ev and ve

--- Circle.v
+++ Circle.v
@@ -14,6 +14,12 @@ Definition S1_ind (P : S1 -> Type) (b : P base) (l : base = base)
     | base => transportf P l b
     end.
 
+Definition S1_recur (A : Type) (b : A) (l : b = b) : S1 -> A :=
+  fun x =>
+    match x with
+    | base => transportf (fun _ => A) l b
+    end.
+
 End S1.
 
 Lemma circle_connected : forall (z: S1), exists p : z = base, True.
@@ -23,6 +29,15 @@ Proof.
   - exists (idpath base).
     exact I.
   - apply loop.
+Qed.
+
+(* Try to prove: forall (z: S1), z = base *)
+Lemma circle_connected' : forall (z: S1), z = base.
+Proof.
+  intro x.
+  apply (S1_ind (fun x => x = base)).
+  - exact (idpath base).
+  - exact (idpath base).
 Qed.
 
 Lemma base_refl : base = base.
@@ -40,3 +55,8 @@ Qed.
 Definition ev (A : Type) : 
   (S1 → A) → ∑ (a : A), a = a :=
   fun g => tpair (fun a => a = a) (g base) (paths_refl (g base)).
+
+Definition ve (A : Type) :
+  (∑ (a : A), a = a) → (S1 → A) :=
+  fun p => S1_recur A (pr1 p) (paths_refl (pr1 p)).
+
