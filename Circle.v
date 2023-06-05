2770d902a24dbd746cedfb89ae6ac286f74f61db
Pallabi Sree Sarker <p.s.p.sarker@student.tudelft.nl>
2023-06-05 15:19:25 +0200

    finished proof ev is an equivalence

--- Circle.v
+++ Circle.v
@@ -22,14 +22,19 @@ Definition S1_ind (P : S1 -> Type) (b : P base) (l : transportf P loop b = b)
     end.
 
 Axiom S1_ind_loop : forall (P : S1 -> Type) (b : P base) (l : transportf P loop b = b),
-    apD (S1_ind P b l) loop = l.
+  apD (S1_ind P b l) loop = l.
 
 Definition S1_recur (A : Type) (a : A) (l : a = a) : S1 -> A :=
   fun x =>
     match x with
-    | base => transportf (fun _ => A) l a
+    | base => a
     end.
+
+Axiom S1_recur_loop : forall (A : Type) (a : A) (l : a = a) ,
+  maponpaths (S1_recur A a l) loop = l.
+
 End S1.
+
 
 Lemma circle_eq_sym : forall (x y : S1), x = y -> y = x.
 Proof.
@@ -53,14 +58,17 @@ Qed.
 
 Definition ev (A : Type) : 
   (S1 → A) → ∑ (a : A), a = a :=
-  fun g => tpair (fun a => a = a) (g base) (paths_refl (g base)).
+  fun g => tpair (fun a => a = a) (g base) (maponpaths g loop).
+
+(* Definition ev' (A : Type) (a : A) :
+  (S1 -> A × a = a) -> (a = a) :=
+  fun t => (pr2 t) @ (maponpaths (pr1 t) loop). *)
 
 Definition ve (A : Type) :
   (∑ (a : A), a = a) → (S1 → A) :=
   fun p => S1_recur A (pr1 p) (pr2 p).
 
-
-Lemma evisweq (A : UU) : isweq (ev A).
+Theorem evisweq (A : UU) : isweq (ev A).
 Proof.
   use isweq_iso.
   - exact (ve A).
@@ -68,12 +76,20 @@ Proof.
     unfold ve.
     unfold ev.
     simpl.
-    apply funext.
-    
+    apply funextfun.
+    use S1_ind.
+    + reflexivity.
+    + Check transportf_paths_FlFr.
+      simpl.
+      rewrite transportf_paths_FlFr.
+      rewrite S1_recur_loop.
+      simpl.
+      use pathsinv0l.
+  - intro p.
+    unfold ev.
+    unfold ve.
+    simpl.
+    rewrite S1_recur_loop.
+    reflexivity.
+Qed.
 
-
-(* TO DO *)
-(* Theorem ev_equiv: Equivalence ev.
-Proof.
-  
-Qed. *)
